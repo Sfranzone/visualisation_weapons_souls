4a45e50b02dc1f4458b65b1e2c01b5d2dce0453c
Olipper <80388772+Olipper@users.noreply.github.com>
2024-06-06 11:58:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,53 +1,367 @@
-// Chemin relatif vers le fichier JSON
-const jsonFilePath = './weapon_category.json';
+let daggers = {
+    "elden ring": "16",
+    "dark souls iii": "13",
+    "sekiro": "0",
+    "remnant ii": "2",
+    "bloodborne": "0",
+    "nioh": "0",
+    "total": "31"
+};
 
-// Déclaration de la variable pour stocker les données
-let weaponData = {};
+let straight_swords = {
+    "elden ring": "19",
+    "dark souls iii": "17",
+    "sekiro": "0",
+    "remnant ii": "6",
+    "bloodborne": "4",
+    "nioh": "0",
+    "total": "46"
+};
 
-// Utilisation de fetch pour charger le fichier JSON
-fetch(jsonFilePath)
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok ' + response.statusText);
-        }
-        return response.json();
-    })
-    .then(data => {
-        // Stocker les données dans la variable
-        weaponData = data;
+let greatswords = {
+    "elden ring": "21",
+    "dark souls iii": "16",
+    "sekiro": "0",
+    "remnant ii": "2",
+    "bloodborne": "3",
+    "nioh": "19",
+    "total": "61"
+};
 
-        // Vérifier les données importées
-        console.log('Données importées :', weaponData);
+let colossal_swords = {
+    "elden ring": "11",
+    "dark souls iii": "11",
+    "sekiro": "0",
+    "remnant ii": "3",
+    "bloodborne": "0",
+    "nioh": "0",
+    "total": "25"
+};
 
-        // Vérification de la structure des données
-        if (weaponData && typeof weaponData === 'object' && !Array.isArray(weaponData)) {
-            console.log('La structure des données est correcte.');
-            // Afficher un message de succès dans le DOM
-            const successMessage = document.createElement('p');
-            successMessage.textContent = 'Importation réussie!';
-            successMessage.style.color = 'green';
-            document.body.appendChild(successMessage);
-        } else {
-            throw new Error('La structure des données est incorrecte.');
-        }
-    })
-    .catch(error => {
-        console.error('There was a problem with the fetch operation:', error);
-        // Afficher un message d'erreur dans le DOM
-        const errorMessage = document.createElement('p');
-        errorMessage.textContent = `Erreur lors de l'importation : ${error.message}`;
-        errorMessage.style.color = 'red';
-        document.body.appendChild(errorMessage);
-    });
+let thrusting_swords = {
+    "elden ring": "7",
+    "dark souls iii": "6",
+    "sekiro": "0",
+    "remnant ii": "0",
+    "bloodborne": "0",
+    "nioh": "0",
+    "total": "13"
+};
 
-// Changer le fill des armes en fonction du pourcentage
+let heavy_thrusting_swords = {
+    "elden ring": "4",
+    "dark souls iii": "0",
+    "sekiro": "0",
+    "remnant ii": "0",
+    "bloodborne": "0",
+    "nioh": "0",
+    "total": "4"
+};
+
+let curved_swords = {
+    "elden ring": "15",
+    "dark souls iii": "15",
+    "sekiro": "0",
+    "remnant ii": "0",
+    "bloodborne": "0",
+    "nioh": "0",
+    "total": "30"
+};
+
+let curved_greatswords = {
+    "elden ring": "9",
+    "dark souls iii": "5",
+    "sekiro": "0",
+    "remnant ii": "0",
+    "bloodborne": "0",
+    "nioh": "0",
+    "total": "14"
+};
+
+let katanas = {
+    "elden ring": "8",
+    "dark souls iii": "8",
+    "sekiro": "2",
+    "remnant ii": "3",
+    "bloodborne": "1",
+    "nioh": "70",
+    "total": "92"
+};
+
+let twinblades = {
+    "elden ring": "6",
+    "dark souls iii": "0",
+    "sekiro": "0",
+    "remnant ii": "0",
+    "bloodborne": "0",
+    "nioh": "0",
+    "total": "6"
+};
+
+let axe = {
+    "elden ring": "13",
+    "dark souls iii": "10",
+    "sekiro": "0",
+    "remnant ii": "2",
+    "bloodborne": "1",
+    "nioh": "29",
+    "total": "55"
+};
+
+let greataxes = {
+    "elden ring": "11",
+    "dark souls iii": "7",
+    "sekiro": "0",
+    "remnant ii": "0",
+    "bloodborne": "1",
+    "nioh": "0",
+    "total": "19"
+};
+
+let hammers = {
+    "elden ring": "15",
+    "dark souls iii": "7",
+    "sekiro": "0",
+    "remnant ii": "3",
+    "bloodborne": "1",
+    "nioh": "0",
+    "total": "26"
+};
+
+let flails = {
+    "elden ring": "5",
+    "dark souls iii": "1",
+    "sekiro": "0",
+    "remnant ii": "2",
+    "bloodborne": "0",
+    "nioh": "0",
+    "total": "8"
+};
+
+let great_hammers = {
+    "elden ring": "14",
+    "dark souls iii": "14",
+    "sekiro": "0",
+    "remnant ii": "3",
+    "bloodborne": "1",
+    "nioh": "0",
+    "total": "32"
+};
+
+let colossal_weapons = {
+    "elden ring": "15",
+    "dark souls iii": "0",
+    "sekiro": "0",
+    "remnant ii": "0",
+    "bloodborne": "1",
+    "nioh": "0",
+    "total": "16"
+};
+
+let spears = {
+    "elden ring": "16",
+    "dark souls iii": "21",
+    "sekiro": "0",
+    "remnant ii": "3",
+    "bloodborne": "1",
+    "nioh": "33",
+    "total": "74"
+};
+
+let great_spears = {
+    "elden ring": "6",
+    "dark souls iii": "0",
+    "sekiro": "0",
+    "remnant ii": "0",
+    "bloodborne": "0",
+    "nioh": "0",
+    "total": "6"
+};
+
+let halberds = {
+    "elden ring": "16",
+    "dark souls iii": "11",
+    "sekiro": "0",
+    "remnant ii": "0",
+    "bloodborne": "0",
+    "nioh": "0",
+    "total": "27"
+};
+
+let reapers = {
+    "elden ring": "4",
+    "dark souls iii": "4",
+    "sekiro": "0",
+    "remnant ii": "2",
+    "bloodborne": "0",
+    "nioh": "0",
+    "total": "10"
+};
+
+let whips = {
+    "elden ring": "6",
+    "dark souls iii": "5",
+    "sekiro": "0",
+    "remnant ii": "0",
+    "bloodborne": "0",
+    "nioh": "0",
+    "total": "11"
+};
+
+let kusarigama = {
+    "elden ring": "0",
+    "dark souls iii": "0",
+    "sekiro": "0",
+    "remnant ii": "0",
+    "bloodborne": "0",
+    "nioh": "28",
+    "total": "28"
+};
+
+let fists = {
+    "elden ring": "9",
+    "dark souls iii": "3",
+    "sekiro": "0",
+    "remnant ii": "1",
+    "bloodborne": "0",
+    "nioh": "15",
+    "total": "28"
+};
+
+let claws = {
+    "elden ring": "4",
+    "dark souls iii": "3",
+    "sekiro": "0",
+    "remnant ii": "5",
+    "bloodborne": "1",
+    "nioh": "0",
+    "total": "13"
+};
+
+let light_bows = {
+    "elden ring": "5",
+    "dark souls iii": "0",
+    "sekiro": "0",
+    "remnant ii": "0",
+    "bloodborne": "0",
+    "nioh": "0",
+    "total": "5"
+};
+
+let bows = {
+    "elden ring": "7",
+    "dark souls iii": "7",
+    "sekiro": "0",
+    "remnant ii": "1",
+    "bloodborne": "0",
+    "nioh": "7",
+    "total": "22"
+};
+
+let greatbows = {
+    "elden ring": "4",
+    "dark souls iii": "3",
+    "sekiro": "0",
+    "remnant ii": "2",
+    "bloodborne": "0",
+    "nioh": "0",
+    "total": "9"
+};
+
+let crossbows = {
+    "elden ring": "7",
+    "dark souls iii": "7",
+    "sekiro": "0",
+    "remnant ii": "2",
+    "bloodborne": "0",
+    "nioh": "0",
+    "total": "16"
+};
+
+let ballistas = {
+    "elden ring": "2",
+    "dark souls iii": "0",
+    "sekiro": "0",
+    "remnant ii": "0",
+    "bloodborne": "0",
+    "nioh": "7",
+    "total": "9"
+};
+
+let glintstone_staffs = {
+    "elden ring": "18",
+    "dark souls iii": "12",
+    "sekiro": "0",
+    "remnant ii": "4",
+    "bloodborne": "0",
+    "nioh": "0",
+    "total": "34"
+};
+
+let sacred_seals = {
+    "elden ring": "9",
+    "dark souls iii": "0",
+    "sekiro": "0",
+    "remnant ii": "0",
+    "bloodborne": "0",
+    "nioh": "0",
+    "total": "9"
+};
+
+let tools = {
+    "elden ring": "11",
+    "dark souls iii": "16",
+    "sekiro": "10",
+    "remnant ii": "0",
+    "bloodborne": "3",
+    "nioh": "0",
+    "total": "40"
+};
+
+let hand_guns = {
+    "elden ring": "0",
+    "dark souls iii": "0",
+    "sekiro": "0",
+    "remnant ii": "21",
+    "bloodborne": "0",
+    "nioh": "0",
+    "total": "21"
+};
+
+let long_guns = {
+    "elden ring": "0",
+    "dark souls iii": "0",
+    "sekiro": "0",
+    "remnant ii": "27",
+    "bloodborne": "0",
+    "nioh": "8",
+    "total": "35"
+};
+
+let trick_weapons = {
+    "elden ring": "0",
+    "dark souls iii": "0",
+    "sekiro": "0",
+    "remnant ii": "0",
+    "bloodborne": "8",
+    "nioh": "0",
+    "total": "8"
+};
+
+let total = {
+    "elden ring": "313",
+    "dark souls iii": "222",
+    "sekiro": "12",
+    "remnant ii": "94",
+    "bloodborne": "26",
+    "nioh": "216",
+    "total": "883"
+};
+
+//Changer le fill des armes en fonction du pourcentage
 function change_fill(percent_weapon, weapon_type) {
     let class_test = document.getElementById(`${weapon_type}`);
-    if (class_test) {
-        class_test.style.clipPath = `rect(${percent_weapon}px 100px 100px 0px)`;
-    } else {
-        console.error(`Element with id ${weapon_type} not found.`);
-    }
+    class_test.style.clipPath = `rect(${percent_weapon}px 100px 100px 0px)`;
 }
 
-change_fill(0, "daggers");
+change_fill(0,"daggers");
